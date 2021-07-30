4c53f717bf254a2f917d7d2aede82c666cd9fb16
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-07-30 07:58:23 -0400

    Docs

--- src/Silk.Core/Services/Bot/Music/MusicState.cs
+++ src/Silk.Core/Services/Bot/Music/MusicState.cs
@@ -11,24 +11,59 @@ namespace Silk.Core.Services.Bot.Music
 {
 	internal sealed record MusicState : IDisposable
 	{
+		//TODO: Volume, Skip(int x), BassBoost?
+		
+		/// <summary>
+		/// The queue'd songs for this particular state.
+		/// </summary>
 		public MusicQueue Queue { get; } = new();
+		
+		/// <summary>
+		/// The currently playing song, if any.
+		/// </summary>
 		public MusicTrack? NowPlaying => Queue.NowPlaying;
+		
+		/// <summary>
+		/// The remaining duration of the song.
+		/// </summary>
 		public TimeSpan RemainingDuration => Queue.RemainingDuration;
 	
-		
+		/// <summary>
+		/// The connection to Discord associated with this state.
+		/// </summary>
 		public VoiceNextConnection Connection { get; init; }
 		
+		/// <summary>
+		/// The channel this state was invoked from. Update messages will be sent back through this channel.
+		/// </summary>
 		public DiscordChannel CommandChannel { get; init; }
+		
+		/// <summary>
+		/// The channel this state is playing music to.
+		/// </summary>
 		public DiscordChannel ConnectedChannel => Connection.TargetChannel;
 
+		/// <summary>
+		/// A public cacellation token used to cancelling CopyToAsync.
+		/// </summary>
 		public CancellationToken Token => _cts.Token;
 		private CancellationTokenSource _cts = new();
-
+		
+		/// <summary>
+		/// Whether this state is in a play state.
+		/// </summary>
 		public bool IsPlaying => _mre.IsSet;
 
 		private readonly AsyncManualResetEvent _mre = new(false);
 
+		/// <summary>
+		/// Source -> FFMpeg
+		/// </summary>
 		public FileStream InStream => (FileStream)_ffmpeg.StandardInput.BaseStream;
+		
+		/// <summary>
+		/// FFMPeg -> VNext
+		/// </summary>
 		public FileStream OutStream => (FileStream)_ffmpeg.StandardOutput.BaseStream;
 		
 		private Process _ffmpeg;
@@ -52,11 +87,20 @@ namespace Silk.Core.Services.Bot.Music
 			RestartFFMpeg();
 			DoDurationLoopAsync();
 		}
-
+		
+		/// <summary>
+		/// Resumes the playback timer.
+		/// </summary>
 		public Task ResumeAsync() => _mre.SetAsync();
-
+		
+		/// <summary>
+		/// An event that fires when the current playing track ends.
+		/// </summary>
 		public event EventHandler TrackEnded;
 		
+		/// <summary>
+		/// Pauses the playback timer and cancels the <see cref="Token"/>.
+		/// </summary>
 		public void Pause()
 		{
 			_mre.Reset();
@@ -64,17 +108,26 @@ namespace Silk.Core.Services.Bot.Music
 			_cts.Dispose();
 			_cts = new();
 		}
-
+		
+		/// <summary>
+		/// Restarts FFMpeg.4
+		/// </summary>
 		public void RestartFFMpeg()
 		{
 			_ffmpeg?.Kill();
 			_ffmpeg?.Dispose();
 			_ffmpeg = Process.Start(_ffmpegInfo)!;
 		}
-
+		
+		/// <summary>
+		/// Loops roughly every second while <see cref="_mre"/> isn't set,
+		/// checking if the current track is about to end, and preloading the next one.
+		///
+		/// <para>This also fires <see cref="TrackEnded"/> which can be used for auto-play.</para>
+		/// </summary>
 		private async void DoDurationLoopAsync()
 		{
-			bool trackLoaded = false;
+			var trackLoaded = false;
 			while (!_disposing)
 			{
 				await _mre.WaitAsync();
@@ -115,7 +168,7 @@ namespace Silk.Core.Services.Bot.Music
 			_cts.Dispose();
 			_ffmpeg?.Kill();
 			_ffmpeg?.Dispose();
-			Connection.Dispose();
+			Connection?.Dispose();
 		}
 	}
 }
